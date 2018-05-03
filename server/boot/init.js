1b5f315fa8ef20cebe782c7082cb1b63f4a2dcde
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-05-03 02:58:17 +0200

    add init node_env

--- server/boot/init.js
+++ server/boot/init.js
@@ -1,30 +1,31 @@
 module.exports = (app) => {
-  app.dataSources.mongo.automigrate('user', (err) => {
-    if (err) throw err;
-
-    const users = [{
-      name: "Average Joe",
-      _ratings: [...Array(5).keys()].map(rating => {
-        return {value: getRandomRating()}
-      })
-    }, {
-      name: "John Doe",
-      _ratings: [...Array(5).keys()].map(rating => {
-        return {value: getRandomRating()}
-      })
-    }, {
-      name: "Alice Avalance",
-      _ratings: [...Array(5).keys()].map(rating => {
-        return {value: getRandomRating()}
-      })
-    }];
-
-    app.models.user.create(users, (err, users) => {
+  if(process.env.NODE_ENV === 'init') {
+    app.dataSources.mongo.automigrate('user', (err) => {
       if (err) throw err;
-      console.log('Models created: ', JSON.stringify(users, null, 2));
-    });
 
-  });
+      const users = [{
+        name: "Average Joe",
+        _ratings: [...Array(5).keys()].map(rating => {
+          return {value: getRandomRating()}
+        })
+      }, {
+        name: "John Doe",
+        _ratings: [...Array(5).keys()].map(rating => {
+          return {value: getRandomRating()}
+        })
+      }, {
+        name: "Alice Avalance",
+        _ratings: [...Array(5).keys()].map(rating => {
+          return {value: getRandomRating()}
+        })
+      }];
+
+      app.models.user.create(users, (err, users) => {
+        if (err) throw err;
+        console.log('Models created: ', JSON.stringify(users, null, 2));
+      });
+    });
+  }
 };
 
 function getRandomRating() {
